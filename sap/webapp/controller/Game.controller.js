f31956b72a7f42c9bfbfe1e16d4e62c7ff168d21
margarita-sh <margaritka1992@gmail.com>
2020-09-19 17:28:51 +0000

    init

--- sap/webapp/controller/Game.controller.js
+++ sap/webapp/controller/Game.controller.js
@@ -5,21 +5,15 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("ns.sap.controller.Game", {
-
-        
             onMakeStep: function (event) {
-                
-
+                let idSAP = event.getSource().getId();
+                //let id = idSAP.split('--').slice(1).join('');
+                let c = document.querySelector('#' + idSAP).classList;
+                if (c.contains('cross')) {
+                    c.remove('cross')
+                } else {
+                    c.add('cross')
+                }
             },
-        
-
-
-            /*      onAfterRendering:function(){
-                     document.querySelectorAll('.box').forEach(item => {
-       item.addEventListener('click', event => {
-          console.log(event.target);
-       })
-     })
-    } */
         });
     });
