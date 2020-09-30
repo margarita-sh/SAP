17e107e706a79ba5a4a1e759b1fcb865aa528cc7
Marharyta <margaritka1992@gmail.com>
2020-09-30 20:59:28 +0000

    init correct game

--- sap/webapp/controller/Game.controller.js
+++ sap/webapp/controller/Game.controller.js
@@ -5,6 +5,9 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("ns.sap.controller.Game", {
+            gameState: 'process',
+            canToClick: true,
+
             gameArray: [
                 ['', '', ''],
                 ['', '', ''],
@@ -20,72 +23,100 @@ sap.ui.define([
                 ['00', '11', '22'],
                 ['02', '11', '20'],
             ],
-            // cellRival : '',
-            // cellUser : '',
 
+            finishGame: function () {
+                this.gameState = 'process';
+                this.gameArray = this.gameArray.map(item => item.map(i => ''));
+                document.querySelectorAll('.zero').forEach(item => item.classList.remove('zero'));
+                document.querySelectorAll('.cross').forEach(item => item.classList.remove('cross'));
+            },
 
             onMakeStep: function (event) {
+                if (this.gameState !== 'process' || this.canToClick == false) {
+                    return;
+                }
+
                 let idSAP = event.getSource().getId();
-                // console.log('idSAP', idSAP);
                 let id = idSAP.split('--').slice(1).join();
                 let coordStep = id.split('_').slice(1).join();
                 let coordString = coordStep.split('-').slice(0, 1).join();
                 let coordColumn = coordStep.split('-').slice(1).join();
-                if (this.gameArray[coordString][coordColumn] === 'X' || this.gameArray[coordString][coordColumn] === '0') {
-                    alert('Выберите другую ячейку');
-                } else {
+                if (this.gameArray[coordString][coordColumn] === '') {
                     this.gameArray[coordString][coordColumn] = 'X';
-                    let cellUser = document.querySelector('#' + idSAP).classList.add('cross');
-                    this.checkWins();
-                    this.onStepRival();
+                    document.querySelector('#' + idSAP).classList.add('cross');
+                    setTimeout(() => {
+                        this.checkWins();
+                        if (this.gameState !== 'process') {
+                            setTimeout(_ => {
+                                this.finishGame();
+                            }, 1000);
+                        } else {
+                            this.onStepRival();
+                        }
+                    }, 0)
+
+                } else {
+                    alert('Выберите другую ячейку');
                 }
-                console.log(this.gameArray);
             },
 
-
-
-              onStepRival: function () {
-                 setTimeout(() => {
-                     for (let i = 0; i < this.gameArray.length; i++) {
-                         for (let j = 0; j < this.gameArray.length; j++) {
-                                 if (this.gameArray[i][j] === '') {
+            onStepRival: function () {
+                this.canToClick = false;
+                setTimeout(() => {
+                    const check = () => {
+                        for (let i = 0; i < this.gameArray.length; i++) {
+                            for (let j = 0; j < this.gameArray.length; j++) {
+                                if (this.gameArray[1][1] == '') {
+                                    this.gameArray[1][1] = '0';
+                                    document.querySelector('#' + `__xmlview0--box_1-1`).classList.add('zero');
+                                    return;
+                                } else if (this.gameArray[i][j] === '') {
                                     this.gameArray[i][j] = '0';
                                     let idCell = `__xmlview0--box_${i}-${j}`;
-                                    console.log(idCell);
-                                    let cellRival = document.querySelector('#' + idCell).classList.add('zero');
+                                    document.querySelector('#' + idCell).classList.add('zero');
+                                    this.checkWins();
                                     return;
-                                } else {
-                                    console.log('something went wrong')
-                                } 
-                         }
-                     }
-                 }, 500);
- 
-             }, 
+                                }
+                            }
+                        }
+                    };
+
+                    check();
+                    this.canToClick = true;
+                }, 500);
+            },
 
             checkWins: function () {
-                let c = [];
+                let winUser = []; //filled with X after each move
+                let winAI = []; //filled with 0 after each move
                 for (let i = 0; i < this.gameArray.length; i++) {
                     for (let j = 0; j < this.gameArray.length; j++) {
                         if (this.gameArray[i][j] === 'X') {
                             let cell_i = i.toString();
                             let cell_j = j.toString();
-                            c.push(cell_i + cell_j);
+                            winUser.push(cell_i + cell_j);
+                        } else if (this.gameArray[i][j] === '0') {
+                            let cell_i = i.toString();
+                            let cell_j = j.toString();
+                            winAI.push(cell_i + cell_j);
                         }
                     }
                 }
 
                 this.winsArray.forEach(element => {
-                     if(JSON.stringify(element) == JSON.stringify(c)){
-                         alert('ТЫ ПОБЕДИИИЛ');
-                        //  console.log(this.cellUser);
-                        // this.cellUser.className.remove();
-                        // this.cellRival.className.remove('zero');
-                     }
-                });
+                    let count = 0;
+                    element.forEach(item => {
+                        if (winUser.indexOf(item) > -1) {
+                            count++
+                        }
+                    })
+                    if (count === 3) {
 
+                        alert('You are win!');
+                        this.gameState = 'end';
+                    }
+                });
             }
 
-           
         });
     });
